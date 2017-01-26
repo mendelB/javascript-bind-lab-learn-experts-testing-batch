6d41eba7c1fc773b018b9904af815810efcd5866
mendelB <mendelblesofsky@gmail.com>
2017-01-26 15:27:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const app = "I don't do much.";
+const digitalClock = {
+	time: Math.round(new Date().getTime() / 1000),
+	startTicking: function(){
+		setInterval(function() {this.time++}.bind(this), 1000)
+	}
+}
+
+function censor(word, string) {
+	let re = new RegExp(word, "g");
+	return string.replace(re, 'BLEEP')
+}
+
+function violenceCensor(string) {
+	return censor.bind(this, 'violence', string)()
+}
+
+const drugsCensor = string => censor.bind(null, 'drugs', string)()
